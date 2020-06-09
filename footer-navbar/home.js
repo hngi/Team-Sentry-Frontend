8844ea7db880521bbe375b813e5923eaf6b09943
Samuel377 <samuelsholademi37@gmail.com>
2020-06-09 10:53:54 +0100

    footer and navbar

---
+++ footer-navbar/home.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("topnav").style.width = "220px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("topnav").style.width = "0";
+  }
