1643ee574eb9f664c655082f46fc49da2bff4f7b
Barnabas Asha <barnabee58@gmail.com>
2020-06-09 14:47:11 +0100

    update

---
+++ Page 3/@codebee@danyel@anny/script.js
@@ -0,0 +1,20 @@
+let menu = document.querySelector(".logo");
+menu.addEventListener('click', (e) => {
+    console.log(22);
+    // const navMenu = document.createElement('div');
+    // navMenu.className = 'navMenu';
+    // const navMenuItems = `
+    // <nav class="nav-container">
+    // <a class="nav-links">HOME</a>
+    // <a class="nav-links">PACKAGES</a>
+    // <a class="nav-links">AMENITIES</a>
+    // <a class="nav-links">CONTACT US</a>
+    // <a class="nav-links">BLOG</a>
+    // </nav>
+    // `;
+    // navMenu.appendChild('navMenuItems')
+    // body.appendChild('navMenu')
+})
+
+const body = document.querySelector('header')
+
